7567394f9797351c269b7feb7543d146ae6d417a
Tiago Lisboa <tiago.caio.ol@gmail.com>
2020-03-26 18:43:52 -0300

    stylished devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 
   # Custom fields
   field :name,               type: String, default: ""
